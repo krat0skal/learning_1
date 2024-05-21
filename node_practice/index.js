bcb6838d9dc380ad4edc99123687cf2051779714
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-05-21 16:54:41 +0700

    ewgsg s

--- node_practice/index.js
+++ node_practice/index.js
@@ -1,27 +1,4 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
-
-const password = '4hWuk0wj8CZCPgfT'
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url =
-  `mongodb+srv://pandeyh38:${password}@clusnoter0.efznds8.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
 
 const Note = require('./models/note')
 const express = require('express')
@@ -42,23 +19,6 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('dist'))
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -71,21 +31,21 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    console.log('Entering noteid rquest')
-    const id = Number(request.params.id)
-    console.log(`id ${id}`)
-    const note = notes.find(note => 
-                                    {
-                                        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-                                        return note.id === id
-                                    })
-    if (note){
+  Note.findById(request.params.id)
+    .then(note => {
+
+      if (note) {
         response.json(note)
-    } else {
+      } else {
         response.status(404).end()
-    }
-    
-  })
+      }
+    })
+
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
+})
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
